a5eb4111ac5dd272004f8e482909ec1be6c6fe4d
Charlotte Curtis <ccurtis@mtroyal.ca>
2023-09-19 13:46:29 -0600

    Setting up demo repo and adding lecture 3

---
+++ lec03/cat_story.py
@@ -0,0 +1,7 @@
+name = "Salvador"
+animal = "dog"
+age = 5
+
+# multiple arguments to print are displayed with a space between each
+print("I have a", animal, "named", name)
+print(name, "is", age, "years old.")
